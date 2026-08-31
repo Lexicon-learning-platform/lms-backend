d4bb2f96b147419ff7e473b38ee378c5b13620b9
Alexander Stauch <blacke4dawn@gmail.com>
2026-08-31 13:40:50 +0200

    adding activities service base

--- src/Lms-backend.Api/Controllers/ActivitiesController.cs
+++ src/Lms-backend.Api/Controllers/ActivitiesController.cs
@@ -1,4 +1,7 @@
+using System.Text.Json;
+using Lms_backend.Application.Interfaces;
 using Lms_backend.Application.Models;
+using Lms_backend.Infrastructure.Models;
 using Microsoft.AspNetCore.JsonPatch.SystemTextJson;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,42 +9,49 @@ namespace Lms_backend.Api.Controllers;
 
 [ApiController]
 [Route("api/activities")]
-public class ActivitiesController : ControllerBase
+public class ActivitiesController(IActivitiesService service) : ControllerBase
 {
     // Base activity endpoints
     [HttpGet]
-    public async Task<IActionResult> GetActivities(CancellationToken token = default)
+    public async Task<IActionResult> GetActivities(string? name, string? search, int? page, int? pageSize, CancellationToken token = default)
     {
-        return Ok();
+        var (result, pagination) = await service.GetMany(new SearchParams(name, search), page, pageSize, token);
+        if (pagination != null) Response.Headers.Append("X-Pagination", JsonSerializer.Serialize(pagination));
+        return Ok(result);
     }
 
     [HttpGet("{id}", Name = "GetActivity")]
     public async Task<IActionResult> GetActivity(Guid id, CancellationToken token = default)
     {
-        return Ok();
+        var result = await service.GetOne(id, token);
+        return Ok(result);
     }
 
     [HttpPost]
     public async Task<IActionResult> CreateActivity(ActivityForChangeDto data, CancellationToken token = default)
     {
-        return CreatedAtRoute("GetActivity", new {});
+        var result = await service.Create(data, token);
+        return CreatedAtRoute("GetActivity", new { result.Id }, result);
     }
 
     [HttpPut("{id}")]
     public async Task<IActionResult> UpdateActivity(Guid id, ActivityForChangeDto data, CancellationToken token = default)
     {
+        await service.Update(id, data, token);
         return NoContent();
     }
 
     [HttpPatch("{id}")]
     public async Task<IActionResult> PatchActivity(Guid id, JsonPatchDocument<ActivityForChangeDto> data, CancellationToken token = default)
     {
+        await service.Update(id, data, token);
         return NoContent();
     }
 
     [HttpDelete("{id}")]
     public async Task<IActionResult> RemoveActivity(Guid id, CancellationToken token = default)
     {
+        await service.Remove(id, token);
         return NoContent();
     }
 
@@ -61,24 +71,28 @@ public class ActivitiesController : ControllerBase
     [HttpPost("{id}/resources")]
     public async Task<IActionResult> CreateActivityResource(Guid id, ResourceForChangeDto data, CancellationToken token = default)
     {
-        return CreatedAtRoute("GetActivityResource", new {});
+        var result = service.AddResource(id, data, token);
+        return CreatedAtRoute("GetActivityResource", new { id, result.Id }, result);
     }
 
     [HttpPut("{id}/resources/{resourceId}")]
     public async Task<IActionResult> UpdateActivityResource(Guid id, Guid resourceId, ResourceForChangeDto data, CancellationToken token = default)
     {
+        await service.UpdateResource(id, resourceId, data, token);
         return NoContent();
     }
 
     [HttpPatch("{id}/resources/{resourceId}")]
     public async Task<IActionResult> PatchActivityResource(Guid id, Guid resourceId, JsonPatchDocument<ResourceForChangeDto> data, CancellationToken token = default)
     {
+        await service.UpdateResource(id, resourceId, data, token);
         return NoContent();
     }
 
     [HttpDelete("{id}/resources/{resourceId}")]
     public async Task<IActionResult> RemoveActivityResource(Guid id, Guid resourceId, CancellationToken token = default)
     {
+        await service.RemoveResource(id, resourceId, token);
         return NoContent();
     }
 }
---
+++ src/Lms-backend.Application/Interfaces/IActivitiesService.cs
@@ -0,0 +1,20 @@
+using Lms_backend.Application.Models;
+using Lms_backend.Domain.Constants;
+using Lms_backend.Infrastructure.Models;
+using Microsoft.AspNetCore.JsonPatch.SystemTextJson;
+
+namespace Lms_backend.Application.Interfaces;
+
+public interface IActivitiesService
+{
+    Task<(IEnumerable<ActivityDto>, PaginationMetadata?)> GetMany(SearchParams searchParams, int? page = DefaultValues.page, int? pageSize = DefaultValues.pageSize, CancellationToken token = default);
+    Task<ActivityExtendedDto> GetOne(Guid id, CancellationToken token = default);
+    Task<ActivityDto> Create(ActivityForChangeDto data, CancellationToken token = default);
+    Task Update(Guid id, ActivityForChangeDto data, CancellationToken token = default);
+    Task Update(Guid id, JsonPatchDocument<ActivityForChangeDto> data, CancellationToken token = default);
+    Task Remove(Guid id, CancellationToken token = default);
+    Task<ResourceDto> AddResource(Guid id, ResourceForChangeDto data, CancellationToken token = default);
+    Task UpdateResource(Guid id, Guid resourceId, ResourceForChangeDto data, CancellationToken token = default);
+    Task UpdateResource(Guid id, Guid resourceId, JsonPatchDocument<ResourceForChangeDto> data, CancellationToken token = default);
+    Task RemoveResource(Guid id, Guid resourceId, CancellationToken token = default);
+}
