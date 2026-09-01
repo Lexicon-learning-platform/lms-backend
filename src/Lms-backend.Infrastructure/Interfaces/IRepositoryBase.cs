d85cd0126fd58e0bcc23029156abedfe0c7821e6
Alexander Stauch <blacke4dawn@gmail.com>
2026-09-01 11:04:27 +0200

    extending RepositoryBase with resource handling

--- src/Lms-backend.Infrastructure/Interfaces/IRepositoryBase.cs
+++ src/Lms-backend.Infrastructure/Interfaces/IRepositoryBase.cs
@@ -1,10 +1,15 @@
+using Lms_backend.Domain.Entities;
+
 namespace Lms_backend.Infrastructure.Interfaces;
 
-public interface IRepositoryBase<TEntity, TResource>
+public interface IRepositoryBase<TEntity, TJoin>
 {
     Task<bool> ExistsAsync(Guid id, CancellationToken token);
     Task<IList<Guid>> GetMissingIdsAsync(ICollection<Guid> ids, CancellationToken token);
     Task<bool> SaveChangesAsync(TEntity entity, CancellationToken token);
     Task AddAsync(TEntity entity, CancellationToken token);
     void Delete(TEntity entity);
+    Task<IList<Resource>> GetResourcesAsync(Guid id, CancellationToken token);
+    Task<bool> AttachResourceAsync(Guid id, Guid resourseId, CancellationToken token);
+    void DetachResource(Guid id, Guid resourceId);
 }
--- src/Lms-backend.Infrastructure/Services/RepositoryBase.cs
+++ src/Lms-backend.Infrastructure/Services/RepositoryBase.cs
@@ -1,23 +1,34 @@
+using Lms_backend.Domain.Entities;
 using Lms_backend.Domain.Interfaces;
 using Lms_backend.Infrastructure.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace Lms_backend.Infrastructure.Services;
 
-public abstract class RepositoryBase<TEntity, TResource>(AppDbContext context) : IRepositoryBase<TEntity, TResource> where TEntity : class, IEntity where TResource : class, IEntity
+public abstract class RepositoryBase<TEntity, TJoin>(AppDbContext context) : IRepositoryBase<TEntity, TJoin> where TEntity : class, IEntity where TJoin : class, IEntity
 {
     protected AppDbContext Context { get; } = context;
     protected abstract DbSet<TEntity> Set { get; }
-    protected abstract DbSet<TResource> ResourceSet { get; }
+    protected abstract DbSet<TJoin> ResourceSet { get; }
 
     public async Task AddAsync(TEntity entity, CancellationToken token)
     {
         await Set.AddAsync(entity, token);
     }
 
+    public Task<bool> AttachResourceAsync(Guid id, Guid resourseId, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
     public void Delete(TEntity entity)
     {
         Set.Remove(entity);
+    }
+
+    public void DetachResource(Guid id, Guid resourceId)
+    {
+        throw new NotImplementedException();
     }
 
     public Task<bool> ExistsAsync(Guid id, CancellationToken token)
@@ -29,6 +40,11 @@ public abstract class RepositoryBase<TEntity, TResource>(AppDbContext context) :
     {
         var found = await Set.Where(e => ids.Contains(e.Id)).Select(e => e.Id).ToListAsync(token);
         return [.. ids.Except(found)];
+    }
+
+    public Task<IList<Resource>> GetResourcesAsync(Guid id, CancellationToken token)
+    {
+        throw new NotImplementedException();
     }
 
     public async Task<bool> SaveChangesAsync(TEntity entity, CancellationToken token)
