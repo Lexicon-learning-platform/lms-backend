a11c0506970dcc853241d48022854d732bf84fae
Alexander Stauch <blacke4dawn@gmail.com>
2026-09-07 10:27:23 +0200

    extending resource validator

--- src/Lms-backend.Application/Validators/ResourceValidator.cs
+++ src/Lms-backend.Application/Validators/ResourceValidator.cs
@@ -1,5 +1,6 @@
 using Lms_backend.Application.Exceptions;
 using Lms_backend.Application.Models;
+using Lms_backend.Domain.Enums;
 
 namespace Lms_backend.Application.Validators;
 
@@ -9,5 +10,7 @@ public static class ResourceValidator
     {
         if (dto.Name.Length < 3 || dto.Name.Length > 50) throw new ValidationException("Resource name has invalid length, must be between 3 and 50 character");
         if (dto.Description.Length < 3 || dto.Description.Length > 200) throw new ValidationException("Resource description has invalid length, must be between 3 and 200 character");
+        if (!ResourceType.IsDefined(dto.Type)) throw new ValidationException($"Unknown resource type: {dto.Type}");
+        if (dto.Data.Length > 2000) throw new ValidationException("Resource data is too large, can't exceed 2000 characters");
     }
 }
