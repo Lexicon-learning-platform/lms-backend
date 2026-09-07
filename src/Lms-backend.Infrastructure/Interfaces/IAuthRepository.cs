37349be5b30f6f18677951adc9286cebd1931067
Jonatan Streith <jonatan@streith.se>
2026-09-07 15:38:06 +0200

    Created basic AuthRepository.

---
+++ src/Lms-backend.Infrastructure/Interfaces/IAuthRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lms_backend.Infrastructure.Interfaces
+{
+    public interface IAuthRepository
+    {
+    }
+}
---
+++ src/Lms-backend.Infrastructure/Services/AuthRepository.cs
@@ -0,0 +1,11 @@
+﻿using Lms_backend.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lms_backend.Infrastructure.Services
+{
+    public class AuthRepository(AppDbContext context) : IAuthRepository
+    {
+    }
+}
