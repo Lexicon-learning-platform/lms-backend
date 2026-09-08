51101572bf82b45ccdfa72d6630085d0a97cacd5
Jonatan Streith <jonatan@streith.se>
2026-09-08 15:13:00 +0200

    Added model for registering.

---
+++ src/Lms-backend.Domain/Entities/RegisterModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lms_backend.Domain.Entities
+{
+    public class RegisterModel
+    {
+        public string GivenName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
