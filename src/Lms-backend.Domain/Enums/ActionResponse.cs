07b845482e1a9776be86b0729edd4143ef4a3bab
Jonatan Streith <jonatan@streith.se>
2026-09-08 16:11:32 +0200

    Added an enum to return what happened with actions.

---
+++ src/Lms-backend.Domain/Enums/ActionResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lms_backend.Domain.Enums
+{
+    public enum ActionResponse
+    {
+        Success,
+        Failure,
+        NotFound,
+        Unauthorized,
+        BadRequest,
+        UserAlreadyExists,
+        UserNotFound,
+        PasswordMismatch
+    }
+}
