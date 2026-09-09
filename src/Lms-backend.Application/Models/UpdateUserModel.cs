37cc8fee8d83b51c70cdad6850e6e88a0747367c
Jonatan Streith <jonatan@streith.se>
2026-09-09 10:20:06 +0200

    Moved model classes to proper Dto folder.

---
+++ src/Lms-backend.Application/Models/UpdateUserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lms_backend.Domain.Entities
+{
+    public class UpdateUserModel
+    {
+        public string? Username { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Role { get; set; }
+        public string? Password { get; set; }
+    }
+}
---
+++ src/Lms-backend.Application/Models/UserStats.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lms_backend.Domain.Entities
+{
+    public record UserStats
+    {
+
+    }
+}
