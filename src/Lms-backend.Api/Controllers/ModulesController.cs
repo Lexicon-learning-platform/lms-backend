eae548fd3f08c90fb39d58756d07b07d7a374675
Alexander Stauch <blacke4dawn@gmail.com>
2026-09-09 12:34:42 +0200

    fixing fetching modules

--- src/Lms-backend.Api/Controllers/ModulesController.cs
+++ src/Lms-backend.Api/Controllers/ModulesController.cs
@@ -13,7 +13,7 @@ public class ModulesController(IModulesService service) : ControllerBase
 {
     // Base modules endpoints
     [HttpGet]
-    public async Task<IActionResult> GetModules(string? name, string? search, Guid course, int? page, int? pageSize, CancellationToken token = default)
+    public async Task<IActionResult> GetModules(string? name, string? search, Guid? course, int? page, int? pageSize, CancellationToken token = default)
     {
         var (result, pagination) = await service.GetMany(new ModuleSearchParams(name, search, course), page, pageSize, token);
         if (pagination != null) Response.Headers.Append("X-Pagination", JsonSerializer.Serialize(pagination));
