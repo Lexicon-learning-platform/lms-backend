43f6832b78c0e45933945117c1e349b17b97d56b
Alexander Stauch <blacke4dawn@gmail.com>
2026-09-09 11:10:29 +0200

    adjusting module DTOs

--- src/Lms-backend.Application/Models/ModuleDto.cs
+++ src/Lms-backend.Application/Models/ModuleDto.cs
@@ -3,9 +3,9 @@ namespace Lms_backend.Application.Models;
 public class ModuleDto
 {
     public Guid Id { get; set; }
-    public string Title { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public int Duration { get; set; }
-    public ActivitySimpleDto[] Activities { get; set; } = [];
+    public IEnumerable<ActivitySimpleDto> Activities { get; set; } = [];
 
 }
--- src/Lms-backend.Application/Models/ModuleForChangeDto.cs
+++ src/Lms-backend.Application/Models/ModuleForChangeDto.cs
@@ -2,9 +2,8 @@ namespace Lms_backend.Application.Models;
 
 public class ModuleForChangeDto
 {
-    public string Title { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public DateOnly StartDate { get; set; }
     public int Duration { get; set; }
     public Guid[] ActivityIds { get; set; } = [];
 }
