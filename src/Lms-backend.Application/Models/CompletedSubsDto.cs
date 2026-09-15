7dfdb9752ef38682f6d1f4e9a0dfde606636363c
Jonatan Streith <jonatan@streith.se>
2026-09-15 16:33:24 +0200

    Skapat endpoint för CheckForCompletion.

---
+++ src/Lms-backend.Application/Models/CompletedSubsDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lms_backend.Application.Models
+{
+    public class CompletedSubsDto
+    {
+        public List<string> CompletedSubs { get; set; } = new List<string>();
+
+        public List<string> UnCompletedSubs { get; set; } = new List<string>();
+
+
+    }
+}
