cdff00f21820af4fc0e8d0ef41c0e7f4cbe48d57
Alexander Stauch <blacke4dawn@gmail.com>
2026-09-16 08:41:17 +0200

    adding DTO for turnins

---
+++ src/Lms-backend.Application/Models/TurninDto.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using Lms_backend.Domain.Enums;
+
+namespace Lms_backend.Application.Models;
+
+public class TurninDto
+{
+    public Guid Id { get; set; }
+    public UserSimpleDto CreatedBy { get; set; } = default!;
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public ResourceType Type { get; set; }
+    public string Data { get; set; } = string.Empty;
+    public Guid ActivityId { get; set; }
+}
