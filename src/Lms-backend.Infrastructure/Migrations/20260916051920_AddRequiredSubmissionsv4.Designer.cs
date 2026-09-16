4d5e631bd795411bd389a1ae09638adc239ba209
Jonatan Streith <jonatan@streith.se>
2026-09-16 08:21:59 +0200

    Migrations

--- src/Lms-backend.Infrastructure/Migrations/20260916051920_AddRequiredSubmissionsv4.Designer.cs
+++ src/Lms-backend.Infrastructure/Migrations/20260916051920_AddRequiredSubmissionsv4.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Lms_backend.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Lms_backend.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260916051920_AddRequiredSubmissionsv4")]
+    partial class AddRequiredSubmissionsv4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
